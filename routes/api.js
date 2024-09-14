c05911b8fb00f474f2f4bd68ff92306273a80b25
winningMove <dariopapic+github@gmail.com>
2024-09-14 10:32:14 +0200

    add model class

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,25 @@
-'use strict';
+"use strict";
+const Issue = require("../repository/Issue");
 
 module.exports = function (app) {
+  const projects = [[]];
 
-  app.route('/api/issues/:project')
-  
-    .get(function (req, res){
+  app
+    .route("/api/issues/:project")
+
+    .get(function (req, res) {
       let project = req.params.project;
-      
     })
-    
-    .post(function (req, res){
+
+    .post(function (req, res) {
       let project = req.params.project;
-      
     })
-    
-    .put(function (req, res){
+
+    .put(function (req, res) {
       let project = req.params.project;
-      
     })
-    
-    .delete(function (req, res){
+
+    .delete(function (req, res) {
       let project = req.params.project;
-      
     });
-    
 };
